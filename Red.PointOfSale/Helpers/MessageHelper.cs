82d1ced55fa89d5744bf15697904e2bf42809169
Ian <ian.escarro@gmail.com>
2019-10-10 23:50:24 +0800

    Message helper

---
+++ Red.PointOfSale/Helpers/MessageHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Red.PointOfSale.Helpers
+{
+    public class MessageHelper
+    {
+        public static void ShowWarning(string text)
+        {
+            MessageBox.Show(MainForm.Instance, text, "Warning", MessageBoxButtons.OK);
+        }
+    }
+}
