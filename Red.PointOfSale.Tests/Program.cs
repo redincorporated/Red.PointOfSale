c30a40c7cd47e0b21d18ec267612e345270fc98c
Ian Escarro <ian.escarro@gmail.com>
2020-01-20 12:04:33 +0800

    Added tests

---
+++ Red.PointOfSale.Tests/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ian
+ * Date: 20/01/2020
+ * Time: 12:01 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Red.PointOfSale.Tests
+{
+    /// <summary>
+    /// Class with program entry point.
+    /// </summary>
+    internal sealed class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+        
+    }
+}
