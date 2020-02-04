2b89c501487f323d9f61758de2a537760161aa43
Ian Escarro <ian.escarro@gmail.com>
2020-02-04 14:17:36 +0800

    Merged base sql repositories

--- Red.PointOfSale/Repositories/IBaseRepository.cs
+++ Red.PointOfSale/Repositories/IBaseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace Red.PointOfSale.Repositories
 {
@@ -10,5 +11,127 @@ namespace Red.PointOfSale.Repositories
         void Delete(int id);
         T Read(int id);
         List<T> FindAll();
+    }
+    
+    public class BaseRepository<T> : IBaseRepository<T>
+    {
+        IDbConnection conn;
+        
+        public BaseRepository(IDbConnection conn)
+        {
+            this.conn = conn;
+        }
+        
+        public virtual void Save(T t)
+        {
+        }
+        
+        public virtual void Update(T t, int id)
+        {
+        }
+        
+        public virtual void Delete(int id)
+        {
+        }
+        
+        public virtual T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public virtual List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void OpenConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Closed) {
+                conn.Open();
+            }
+        }
+
+        protected double GetDouble(IDataReader rs, int index)
+        {
+            return GetDouble(rs, index, 0);
+        }
+
+        protected double GetDouble(IDataReader rs, int index, double _default)
+        {
+            return !rs.IsDBNull(index) ? rs.GetDouble(index) : _default;
+        }
+
+        protected DateTime GetDateTime(IDataReader rs, int index)
+        {
+            return GetDateTime(rs, index, DateTime.MinValue);
+        }
+
+        protected DateTime GetDateTime(IDataReader rs, int index, DateTime _default)
+        {
+            return !rs.IsDBNull(index) ? rs.GetDateTime(index) : _default;
+        }
+
+        protected int GetInt32(IDataReader rs, int index)
+        {
+            return GetInt32(rs, index, 0);
+        }
+
+        protected int GetInt32(IDataReader rs, int index, int _default)
+        {
+            return !rs.IsDBNull(index) ? rs.GetInt32(index) : _default;
+        }
+
+        protected string GetString(IDataReader rs, int index)
+        {
+            return GetString(rs, index, "");
+        }
+
+        protected string GetString(IDataReader rs, int index, string _default)
+        {
+            return !rs.IsDBNull(index) ? rs.GetString(index) : _default;
+        }
+
+        protected object ExecuteScalar(string query, params IDbDataParameter[] parameters)
+        {
+            var cmd = conn.CreateCommand();
+            OpenConnection();
+            cmd.CommandText = query;
+            foreach (var param in parameters) {
+                cmd.Parameters.Add(param);
+            }
+            var returnValue = cmd.ExecuteScalar();
+            CloseConnection();
+            return returnValue;
+        }
+
+        protected void ExecuteNonQuery(string query, params IDbDataParameter[] parameters)
+        {
+            var cmd = conn.CreateCommand();
+            OpenConnection();
+            cmd.CommandText = query;
+            foreach (var param in parameters) {
+                cmd.Parameters.Add(param);
+            }
+            cmd.ExecuteNonQuery();
+            CloseConnection();
+        }
+
+        protected IDataReader ExecuteReader(string query, params IDbDataParameter[] parameters)
+        {
+            var cmd = conn.CreateCommand();
+            OpenConnection();
+            cmd.CommandText = query;
+            foreach (var param in parameters) {
+                cmd.Parameters.Add(param);
+            }
+            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        protected void CloseConnection()
+        {
+            if (conn.State == System.Data.ConnectionState.Open) {
+                conn.Close();
+            }
+        }
     }
 }
--- Red.PointOfSale/Repositories/MySql/BaseMySqlRepository.cs
+++ Red.PointOfSale/Repositories/MySql/BaseMySqlRepository.cs
@@ -6,69 +6,10 @@ using System.Configuration;
 
 namespace Red.PointOfSale.Repositories.MySql
 {
-    public class BaseMySqlRepository<T> : IBaseRepository<T>
+    public class BaseMySqlRepository<T> : BaseRepository<T>
     {
-        IDbConnection conn = new MySqlConnection(ConfigurationManager.AppSettings["mysql-connection"]);
-        
-        public BaseMySqlRepository()
+        public BaseMySqlRepository() : base(new MySqlConnection(ConfigurationManager.AppSettings["mysql-connection"]))
         {
-        }
-        
-        public string GetString(IDataReader rs, int index)
-        {
-            return GetString(rs, index, "");
-        }
-        
-        public string GetString(IDataReader rs, int index, string @default)
-        {
-            return !rs.IsDBNull(index) ? rs.GetString(index) : @default;
-        }
-        
-        public IDataReader ExecuteReader(string query, params IDbDataParameter[] parameters)
-        {
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = query;
-            foreach (var p in parameters) {
-                cmd.Parameters.Add(p);
-            }
-            OpenConnection();
-            return cmd.ExecuteReader(CommandBehavior.CloseConnection);
-        }
-        
-        protected void OpenConnection()
-        {
-            if (conn.State == ConnectionState.Closed) {
-                conn.Open();
-            }
-        }
-        
-        protected void CloseConnection()
-        {
-            if (conn.State == ConnectionState.Open) {
-                conn.Close();
-            }
-        }
-        
-        public virtual void Save(T t)
-        {
-        }
-        
-        public virtual void Update(T t, int id)
-        {
-        }
-        
-        public virtual void Delete(int id)
-        {
-        }
-        
-        public virtual T Read(int id)
-        {
-            throw new NotImplementedException();
-        }
-        
-        public virtual List<T> FindAll()
-        {
-            throw new NotImplementedException();
         }
     }
 }
